77924bc6e120dc063b2a867f51af12f11414de3c
Arthur <37119842+ArthurWest@users.noreply.github.com>
2019-06-06 22:29:03 +0300

    You can now play (kind of)
    You can now play this game, although it does not all the functionality of the original checkers yet.

--- czech.js
+++ czech.js
@@ -10,11 +10,38 @@ class Czech {
 
 	}
 	hasAttack(grid) {
-		//First one
-		if (grid[this.x-1][this.y-1] != false && grid[this.x-1][this.y-1].czech && grid[this.x-2][this.y-2].czech == null) {
-			return 1
-		} else if (grid[this.x+1][this.y-1].czech && grid[this.x+2][this.y-2].czech == null) {
-			return 2
+		//
+		const x = this.x
+		const y = this.y
+
+		//Creating array for the result
+		const options = []
+		for (let i = 0; i < options.length; i++) {
+			options[i] = null
 		}
+
+		//Top right
+		if (( x+2 >= 0 && x+2 <= 7 ) && ( y-2 >= 0 && y-2 <=7) && grid[x+1][y-1].white != this.white) { //1 & 2 is checking if the cell exists; 3 is checking if it's other player's chezch
+			options[0] = grid[x+2][y-2]
+		}
+		//Bottom right
+		if (( x+2 >= 0 && x+2 <= 7 ) && ( y+2 >= 0 && y+2 <=7) && grid[x+1][y+1].white != this.white) { //1 & 2 is checking if the cell exists; 3 is checking if it's other player's chezch
+			options[1] = grid[x+2][y+2]
+		}
+		//Bottom left
+		if (( x-2 >= 0 && x-2 <= 7 ) && ( y+2 >= 0 && y+2 <=7) && grid[x-1][y+1].white != this.white) { //1 & 2 is checking if the cell exists; 3 is checking if it's other player's chezch
+			options[2] = grid[x-2][y+2]
+		}
+		//Top left 
+		if (( x-2 >= 0 && x-2 <= 7 ) && ( y-2 >= 0 && y-2 <=7) && grid[x-1][y-1].white != this.white) { //1 & 2 is checking if the cell exists; 3 is checking if it's other player's chezch
+			options[3] = grid[x-2][y-2]
+		}
+		//Result
+		if (options) {
+			return options
+		} else {
+			return false
+		}
+		
 	}
 }
